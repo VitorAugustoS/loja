f3e22e57fa32747d3c957d7b93840fcbecec69ed
Vitor <vittoraug@gmail.com>
2022-05-24 20:21:18 -0300

    JQuery Mask

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -11,7 +11,7 @@
 		</div>
 		<div class="form-group col-5">
 			<label for="cpf">CPF:</label>
-			<input type="text" name="cpf" class="form-control" value="{{ $cliente->cpf }}" />
+			<input type="text" id="cpf" name="cpf" class="form-control" value="{{ $cliente->cpf }}" />
 		</div>
 		<div class="form-group col-2">
 			@csrf
@@ -31,3 +31,52 @@
 		</div>
 	</form>
 @endsection
+
+@section("tabela")
+	<table class="table table-striped" style="margin-top: 50px;">
+		<colgroup>
+			<col width="200">
+			<col width="200">
+			<col width="100">
+			<col width="100">
+		</colgroup>
+		<thead>
+			<tr>
+				<th>Nome</th>
+				<th>CPF</th>
+				<th>Edit</th>
+				<th>Del</th>
+			</tr>
+		</thead>
+		<tbody>
+			@foreach ($clientes as $cliente)
+				<tr>
+					<td>{{ $cliente->nome }}</td>
+					<td>{{ $cliente->cpf }}</td>
+					<td>
+						<a href="/cliente/{{ $cliente->id }}/edit" class="btn btn-warning">
+							<i class="bi bi-pencil-square"></i>
+							Edit
+						</a>
+					</td>
+					<td>
+						<form action="/cliente/{{ $cliente->id }}" method="POST">
+							@csrf
+							<input type="hidden" name="_method" value="DELETE" />
+							<button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza?');">
+								<i class="bi bi-trash"></i>
+								Del
+							</button>
+						</form>
+					</td>
+				</tr>
+			@endforeach
+		</tbody>
+	</table>
+@endsection
+
+<script>
+	document.addEventListener("DOMContentLoaded", function() {
+		$("#cpf").mask("000.000.000-00", {"placeholder": "___.___.___-__"});
+	});
+</script>
