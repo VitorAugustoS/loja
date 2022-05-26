e288f0a5224782c753505970b52a0999b9121236
Vitor <vittoraug@gmail.com>
2022-05-26 20:24:13 -0300

    Tela de Clientes

--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -4,14 +4,15 @@
 
 @section("formulario")
 	<h1>Cadastro de Clientes</h1>
-	<form action="/cliente" method="POST" class="row">
+	<form action="/cliente" method="POST" class="row" onsubmit="carregaDados();">
 		<div class="form-group col-5">
 			<label for="nome">Nome:</label>
 			<input type="text" name="nome" class="form-control" value="{{ $cliente->nome }}" />
 		</div>
 		<div class="form-group col-5">
 			<label for="cpf">CPF:</label>
-			<input type="text" id="cpf" name="cpf" class="form-control" value="{{ $cliente->cpf }}" />
+			<input type="text" id="cpf_mask" name="cpf_mask" class="form-control" value="{{ $cliente->cpf }}" />
+			<input type="hidden" id="cpf" name="cpf" value="{{ $cliente->cpf }}" />
 		</div>
 		<div class="form-group col-2">
 			@csrf
@@ -33,7 +34,12 @@
 @endsection
 
 @section("tabela")
-	<table class="table table-striped" style="margin-top: 50px;">
+	<div class="row" style="margin-top: 50px;">
+		<div class="col-12 form-group">
+			<input type="text" id="q" placeholder="Pesquisar por nome..." class="form-control" onkeyup="buscar($(this).val());" />
+		</div>
+	</div>
+	<table id="tabClientes" class="table table-striped" style="margin-top: 10px;">
 		<colgroup>
 			<col width="200">
 			<col width="200">
@@ -51,8 +57,8 @@
 		<tbody>
 			@foreach ($clientes as $cliente)
 				<tr>
-					<td>{{ $cliente->nome }}</td>
-					<td>{{ $cliente->cpf }}</td>
+					<td class="td_nome">{{ $cliente->nome }}</td>
+					<td class="td_cpf">{{ $cliente->cpf }}</td>
 					<td>
 						<a href="/cliente/{{ $cliente->id }}/edit" class="btn btn-warning">
 							<i class="bi bi-pencil-square"></i>
@@ -76,7 +82,38 @@
 @endsection
 
 <script>
+
+
+	function buscar(q){
+		
+		q = q.toLowerCase();
+		
+		$("#tabClientes tbody tr").each(function(){
+			
+			var mostrar = true;
+			
+			var nome = $("td.td_nome", this).html();
+			nome = nome.toLowerCase();
+			
+			var cpf = $("td.td_cpf", this).cleanVal();
+			
+			mostrar = nome.includes(q) || cpf.includes(q);
+			
+			if (mostrar){
+				$(this).show();
+			} else {
+				$(this).hide();
+			}
+			
+		});
+	}
+
+	function carregaDados(){
+		$("#cpf").val($("#cpf_mask").cleanVal());
+	}
+
 	document.addEventListener("DOMContentLoaded", function() {
-		$("#cpf").mask("000.000.000-00", {"placeholder": "___.___.___-__"});
+		$("#cpf_mask").mask("000.000.000-00", {"placeholder": "___.___.___-__"});
+		$(".td_cpf").mask("000.000.000-00");
 	});
 </script>
